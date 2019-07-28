5eef8af73d8d2185e520d4a6f1cb6ce183a7c4a2
purba500 <richard.purba@kpn.com>
2019-07-28 20:59:52 +0200

    cleaning up

--- src/main/java/RandomNumberGenerator.java
+++ src/main/java/RandomNumberGenerator.java
@@ -1,7 +1,5 @@
 import com.google.gson.Gson;
-import org.json.JSONObject;
 import java.io.BufferedReader;
-import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
